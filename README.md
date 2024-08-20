d76de7a232cf607469a379a38e718743c57618b9
joppe <joppe@pop-os.localdomain>
2024-08-21 00:08:15 +0200

    Got the basic poll setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://stackoverflow.com/a/3233022
+https://www.baeldung.com/cs/tcp-fin-vs-rst
--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,13 @@
 #include <type_traits>
 #include <unistd.h>
 #include <poll.h>
+#include <vector>
 
 #define LOG_ERROR(x) std::cerr << "[ERROR] : " << x << std::endl;
 #define LOG(x) std::cout << "[INFO] : " << x << std::endl;
 
 #define POLL_TIMEOUT 100
+#define LISTEN_BACKLOG 8
 
 
 bool setup(const uint16_t port, int &serverFd)
@@ -66,7 +68,7 @@ bool setup(const uint16_t port, int &serverFd)
 		LOG("Bound to port: " << port);
 	}
 
-	if (listen(serverFd, 1) == -1)
+	if (listen(serverFd, LISTEN_BACKLOG) == -1)
 	{
 		LOG_ERROR("Listen failed, " << strerror(errno));
 		close(serverFd);
@@ -105,6 +107,7 @@ void handleConnection(int fd)
 	while (1)
 	{
 		bzero(&buffer, sizeof(buffer));
+		// read up on EWOULDBLOCK
 		recv(fd, buffer, sizeof(buffer), 0);
 		LOG("Received: [" << buffer << "]");
 
@@ -118,43 +121,61 @@ void handleConnection(int fd)
 
 }
 
-int main ()
+int main()
 {
-	int serverFd, clientFd;
+	int serverFd;
 	if (!setup(8080, serverFd))
 	{
 		return 1;
 	}
 
+	std::vector<pollfd> pollFds;
+
+	pollFds.push_back({serverFd, POLLIN, 0});
 
 
 	while (1)
 	{
-		// acceptConnections(serverFd);
-
-		// check for IO on other conns.
-
-		pollfd pfd;
-		pfd.fd = serverFd;
-		pfd.events = POLLIN | POLLOUT;
-		pfd.revents = 0;
-
-		int nReady = poll(&pfd, 1, POLL_TIMEOUT);
+		int nReady = poll(pollFds.data(), pollFds.size(), POLL_TIMEOUT);
 		if (nReady == -1)
 		{
 			LOG_ERROR("Failed polling: " << strerror(errno));
 			return 0;
 		}
 		LOG("nReady: " << nReady);
-		if (pfd.revents & POLLIN)
+
+
+		for (size_t i = 0; i < pollFds.size(); i++)
 		{
-			LOG("Ready: POLLIN");
-			int clientFd = acceptConnections(serverFd);
+			pollfd &pfd = pollFds[i];
+			LOG("checking sock fd: " << pfd.fd);
+
+			// check if current fd is listening socket.
+			int val;
+			socklen_t len = sizeof(val);
+			getsockopt(pfd.fd, SOL_SOCKET, SO_ACCEPTCONN, &val, &len);
+			if (val && pfd.revents)
+			{
+				// Handle listening socket.
+				LOG("event on listen socket " << pfd.fd)
+				int clientFd = acceptConnections(serverFd);
+				pollFds.push_back({clientFd, POLLIN | POLLOUT, 0});
+			}
+
+			else if (pfd.revents)
+			{
+				if (pfd.revents & POLLIN)
+				{
+					LOG("fd: " << pfd.fd << " POLLIN");
+				}
+				if (pfd.revents & POLLOUT)
+				{
+					LOG("fd: " << pfd.fd << " POLLOUT");
+					// std::string msg = "client on fd: " + std::to_string(pfd.fd) + "\n";
+					// send(pfd.fd, msg.c_str(), msg.length() * sizeof(char), 0);
+				}
+			}
 		}
-		if (pfd.revents & POLLOUT)
-			LOG("Ready: POLLOUT");
-
-
 	}
 
 
@@ -162,9 +183,13 @@ int main ()
 
 
 	close(serverFd);
-	close(clientFd);
+	for (pollfd pfd : pollFds)
+	{
+		close(pfd.fd);
+	}
 
 	LOG("Test");
 
 	return 0;
 }
+
